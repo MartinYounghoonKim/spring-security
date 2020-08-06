f05ea65df26a35598f2992e2bf4ab00cf379ee51
martinyounghoonkim <hoons0131@gmail.com>
2020-08-06 10:24:10 +0900

    샘플 컨트롤러 추가

---
+++ src/main/java/com/example/security/controller/SecurityController.java
@@ -0,0 +1,17 @@
+package com.example.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+	@GetMapping
+	public String hello () {
+		return "Hello";
+	}
+
+	@GetMapping("/loginPage")
+	public String loginPage () {
+		return "loginPage";
+	}
+}
